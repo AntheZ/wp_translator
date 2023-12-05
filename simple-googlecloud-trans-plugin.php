559709b4eca59e8a9442debdf521dda83cf09d08
AntheZ <894966@gmail.com>
2023-12-05 19:18:43 +0200

    first version

---
+++ simple-googlecloud-trans-plugin.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Plugin Name: Simple Google Cloud Translation Plugin
+Description: A simple plugin to translate posts using Google Cloud Translation API
+Version: 0.1
+Author: Anton Zanizdra
+*/
+
+// Add a new submenu under Settings
+function mt_add_pages() {
+    add_options_page(__('Translation','menu-test'), __('Translation','menu-test'), 'manage_options', 'translationhandle', 'mt_settings_page');
+}
+add_action('admin_menu', 'mt_add_pages');
+
+// mt_settings_page() displays the page content for the Test settings submenu
+function mt_settings_page() {
+    echo "<h2>" . __( 'Translation Settings', 'menu-test' ) . "</h2>";
+    // place your settings form here 
+}
+
+// Register and define the settings
+add_action('admin_init', 'mt_admin_init');
+function mt_admin_init(){
+    register_setting( 'mt_options', 'mt_options', 'mt_validate_options' );
+    add_settings_section('mt_main', 'Main Settings', 'mt_section_text', 'translationhandle');
+    add_settings_field('mt_text_string', 'Language Code', 'mt_setting_string', 'translationhandle', 'mt_main');
+}
+
+// Draw the section header
+function mt_section_text() {
+    echo '<p>Enter your settings here.</p>';
+}
+
+// Display and fill the form field
+function mt_setting_string() {
+    // get option 'text_string' value from the database
+    $options = get_option( 'mt_options' );
+    $value = $options['text_string'];
+    // echo the field
+    echo "<input id='mt_text_string' name='mt_options[text_string]' type='text' value='$value' />";
+}
+
+// Validate user input
+function mt_validate_options($input) {
+    $valid = array();
+    $valid['text_string'] = sanitize_text_field($input['text_string']);
+    return $valid;
+}
+
+// Connect to Google Cloud Translation API and translate the posts
+function translate_posts() {
+    // Get the language code from the settings
+    $options = get_option( 'mt_options' );
+    $language_code = $options['text_string'];
+
+    // Get all posts
+    $args = array(
+        'posts_per_page'   => -1,
+        'post_type'        => 'post',
+    );
+    $posts_array = get_posts( $args );
+
+    foreach ( $posts_array as $post ) {
+        // Connect to Google Cloud Translation API and translate the post
+        // Make sure to handle the rate limit of 6000000 characters per minute
+        // Log the translation process
+    }
+
+    // Update the progress bar in the admin interface
+}
+
+// Use the translated posts
+function use_translated_posts() {
+    // Backup the original posts and postmeta tables
+    // Replace the original posts and postmeta tables with the translated ones
+    // Log the process
+}
+
+?>
