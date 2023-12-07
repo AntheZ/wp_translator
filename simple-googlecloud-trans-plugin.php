aa71c423f596dd540e4a5bcfb08316a9ead1e8c7
AntheZ <894966@gmail.com>
2023-12-07 09:09:41 +0200

    Version: 0.23

--- simple-googlecloud-trans-plugin.php
+++ simple-googlecloud-trans-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Simple Google Cloud Translation Plugin
 Description: A simple plugin to translate posts using Google Cloud Translation API
-Version: 0.22
+Version: 0.23
 Author: AntheZ
 */
 
@@ -34,7 +34,7 @@ function mt_add_pages() {
 }
 
 function mt_settings_page() {
-    echo "<h2>" . __( 'SGC Translation Settings', 'menu-test' ) . "</h2>";
+    echo "<h2>" . __( 'Sіmple Google Cloud (SGS) Translation Settings', 'menu-test' ) . "</h2>";
     echo '<form action="options.php" method="post">';
     settings_fields('mt_options');
     do_settings_sections('translationhandle');
@@ -49,7 +49,7 @@ function mt_admin_init(){
     add_settings_field('mt_api_key', 'API Key', 'mt_setting_api_key', 'translationhandle', 'mt_main');
     add_settings_section('mt_main_analyse', 'Additional Settings', 'mt_section_text', 'translationhandle');
     add_settings_field('mt_batch_size', 'Batch Size', 'mt_batch_size_input', 'translationhandle', 'mt_main_analyse');
-    add_settings_field('mt_analyse_button', 'Analyse Posts', 'pluginSettingsPage', 'translationhandle', 'mt_main_analyse');
+    add_settings_field('mt_analyse_button', 'Analyse Posts', 'mt_setting_analyse_button', 'translationhandle', 'mt_main_analyse');
     add_settings_section('mt_main_translate', 'Translation Settings', 'mt_section_text', 'translationhandle');
     add_settings_field('mt_website_language_code', 'Website Language Code', 'mt_setting_website_language_code', 'translationhandle', 'mt_main_translate');
     add_settings_field('mt_translation_language_code', 'Translation Language Code', 'mt_setting_translation_language_code', 'translationhandle', 'mt_main_translate');
@@ -69,6 +69,15 @@ function mt_section_text() {
     echo '<p>Тут колись буде опис</p>';
 }
 
+// Функція для відображення поля вводу для налаштування API
+function mt_setting_api_key() {
+    // get option 'api_key' value from the database
+    $options = get_option( 'mt_options' );
+    $value = $options['api_key'];
+    // echo the field
+    echo "<input id='mt_api_key' name='mt_options[api_key]' type='text' value='$value' />";
+}
+
 // Функція для відображення поля вводу для налаштування 'mt_batch_size'
 function mt_batch_size_input() {
     // Отримання поточного значення налаштування 'mt_batch_size'
@@ -78,7 +87,121 @@ function mt_batch_size_input() {
     echo "<input id='mt_batch_size' name='mt_batch_size' type='number' value='" . esc_attr($batch_size) . "' />";
 }
 
-function pluginSettingsPage() {
+// Функція для відображення поля вводу для налаштування мови статей, які потрібно перекласти
+function mt_setting_website_language_code() {
+    // get option 'website_language_code' value from the database
+    $options = get_option( 'mt_options' );
+    $value = $options['website_language_code'];
+    // echo the field
+    echo "<input id='mt_website_language_code' name='mt_options[website_language_code]' type='text' value='$value' />";
+}
+
+// Функція для відображення поля вводу для налаштування мови статей, на яку потрібно перекласти
+function mt_setting_translation_language_code() {
+    // get option 'translation_language_code' value from the database
+    $options = get_option( 'mt_options' );
+    $value = $options['translation_language_code'];
+    // echo the field
+    echo "<input id='mt_translation_language_code' name='mt_options[translation_language_code]' type='text' value='$value' />";
+}
+
+// Перевірка правильності вводу даних користувачем
+function mt_validate_options($input) {
+// List of supported languages by Google Cloud Translation API
+$valid_language_codes = array("af", "sq", "am", "ar", "hy", "as", "ay", "az", "bm", "eu", "be", "bn", "bho", "bs", "bg", "ca", "ceb", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "dv", "doi", "nl", "en", "eo", "et", "ee", "fil", "fi", "fr", "fy", "gl", "ka", "de", "el", "gn", "gu", "ht", "ha", "haw", "he", "hi", "hmn", "hu", "is", "ig", "ilo", "id", "ga", "it", "ja", "jv", "kn", "kk", "km", "rw", "gom", "ko", "kri", "ku", "ckb", "ky", "lo", "la", "lv", "ln", "lt", "lg", "lb", "mk", "mai", "mg", "ms", "ml", "mt", "mi", "mr", "mni-Mtei", "lus", "mn", "my", "ne", "no", "ny", "or", "om", "ps", "fa", "pl", "pt", "pa", "qu", "ro", "ru", "sm", "sa", "gd", "nso", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tl", "tg", "ta", "tt", "te", "th", "ti", "ts", "tr", "tk", "ak", "uk", "ur", "ug", "uz", "vi", "cy", "xh", "yi", "yo", "zu");
+
+    // Check if the language code is in the list of supported languages
+    if (!in_array($input['website_language_code'], $valid_language_codes)) {
+        add_settings_error('mt_options', 'mt_invalid_website_language_code', 'Invalid website language code.');
+    }
+    if (!in_array($input['translation_language_code'], $valid_language_codes)) {
+        add_settings_error('mt_options', 'mt_invalid_translation_language_code', 'Invalid translation language code.');
+    }
+
+    $api_key = $input['api_key'];
+    $url = "https://translation.googleapis.com/language/translate/v2?key=$api_key&q=hello&source=en&target=es";
+    $response = wp_remote_get($url);
+    if ( is_wp_error( $response ) ) {
+        add_settings_error('mt_options', 'mt_connection_error', 'Could not connect to the API. Please check your API key.');
+    }
+    return $input;
+}
+
+// Давайте проаналізуємо скільки саме у нас статей, та їх мову. Але зробимо це безпечно пачками по Х штук
+add_action('wp_ajax_analysePosts', 'analysePosts');
+function analysePosts() {
+    global $wpdb;
+    // Встановлюємо кількість статей для обробки за один раз. 
+    $batch_size = get_option('mt_batch_size', 100); // 100 - значення за замовчуванням
+    // Отримуємо кількість всіх опублікованих статей
+    $total_posts = $wpdb->get_var("SELECT COUNT(*) FROM {$wpdb->prefix}posts WHERE post_type = 'post' AND post_status = 'publish'");
+    // Розраховуємо кількість партій
+    $batches = ceil($total_posts / $batch_size);
+    $start_time = microtime(true);
+
+    for ($i = 0; $i < $batches; $i++) {
+        // Обчислюємо зсув для поточної партії
+        $offset = $i * $batch_size;
+
+        // Отримуємо статті для поточної партії
+        $posts = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}posts WHERE post_type = 'post' AND post_status = 'publish' LIMIT $offset, $batch_size");
+
+        foreach ($posts as $post) {
+            // Визначаємо мову статті
+            $language_code = detectLanguage($post->post_content);
+
+            // Додаємо дані до нової таблиці
+            $wpdb->insert(
+                "{$wpdb->prefix}sgct_analysed_posts",
+                array(
+                    'post_id' => $post->ID,
+                    'post_title' => $post->post_title,
+                    'language_code' => $language_code
+                )
+            );
+        }
+    }
+
+    $end_time = microtime(true);
+    $execution_time = $end_time - $start_time;
+
+    // Повертаємо результати через AJAX
+    echo "Аналіз завершено. Оброблено " . $total_posts . " статей. Витрачено часу " . $execution_time . " секунд.";
+    wp_die(); // це потрібно, щоб уникнути повернення 0 в кінці відповіді AJAX
+}
+
+// Detect language code uk or ru on first 50 words of a post
+function detectLanguage($text) {
+    // Обмежуємо текст до перших 50 слів
+    $words = explode(' ', $text);
+    $text = implode(' ', array_slice($words, 0, 50));
+    // Видаляємо HTML з тексту
+    $text = wp_strip_all_tags($text);
+    // Набір унікальних слів для кожної мови
+    $ukrainianWords = array('і', 'ї', 'є', 'ґ');
+    $russianWords = array('ы', 'э', 'ё', 'й');
+    $ukCount = 0;
+    $ruCount = 0;
+
+    // Перевіряємо кількість унікальних слів для кожної мови в тексті
+    foreach ($ukrainianWords as $word) {
+        $ukCount += substr_count($text, $word);
+    }
+
+    foreach ($russianWords as $word) {
+        $ruCount += substr_count($text, $word);
+    }
+
+    // Повертаємо код мови в залежності від того, яка мова має більшу кількість унікальних слів
+    if ($ukCount > $ruCount) {
+        return 'uk';
+    } else {
+        return 'ru';
+    }
+}
+
+// Налаштування кнопки Аналізу статей
+function mt_setting_analyse_button() {
     // Перевіряємо, чи була натиснута кнопка
     if (isset($_POST['analyse_posts'])) {
         // Виконуємо функцію analysePosts
@@ -110,54 +233,6 @@ function pluginSettingsPage() {
     });
     </script>
     ';
-}
-
-// Display and fill the website language form field
-function mt_setting_website_language_code() {
-    // get option 'website_language_code' value from the database
-    $options = get_option( 'mt_options' );
-    $value = $options['website_language_code'];
-    // echo the field
-    echo "<input id='mt_website_language_code' name='mt_options[website_language_code]' type='text' value='$value' />";
-}
-
-// Display and fill the translation language form field
-function mt_setting_translation_language_code() {
-    // get option 'translation_language_code' value from the database
-    $options = get_option( 'mt_options' );
-    $value = $options['translation_language_code'];
-    // echo the field
-    echo "<input id='mt_translation_language_code' name='mt_options[translation_language_code]' type='text' value='$value' />";
-}
-
-function mt_setting_api_key() {
-    // get option 'api_key' value from the database
-    $options = get_option( 'mt_options' );
-    $value = $options['api_key'];
-    // echo the field
-    echo "<input id='mt_api_key' name='mt_options[api_key]' type='text' value='$value' />";
-}
-
-// Validate user input
-function mt_validate_options($input) {
-// List of supported languages by Google Cloud Translation API
-$valid_language_codes = array("af", "sq", "am", "ar", "hy", "as", "ay", "az", "bm", "eu", "be", "bn", "bho", "bs", "bg", "ca", "ceb", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "dv", "doi", "nl", "en", "eo", "et", "ee", "fil", "fi", "fr", "fy", "gl", "ka", "de", "el", "gn", "gu", "ht", "ha", "haw", "he", "hi", "hmn", "hu", "is", "ig", "ilo", "id", "ga", "it", "ja", "jv", "kn", "kk", "km", "rw", "gom", "ko", "kri", "ku", "ckb", "ky", "lo", "la", "lv", "ln", "lt", "lg", "lb", "mk", "mai", "mg", "ms", "ml", "mt", "mi", "mr", "mni-Mtei", "lus", "mn", "my", "ne", "no", "ny", "or", "om", "ps", "fa", "pl", "pt", "pa", "qu", "ro", "ru", "sm", "sa", "gd", "nso", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tl", "tg", "ta", "tt", "te", "th", "ti", "ts", "tr", "tk", "ak", "uk", "ur", "ug", "uz", "vi", "cy", "xh", "yi", "yo", "zu");
-
-    // Check if the language code is in the list of supported languages
-    if (!in_array($input['website_language_code'], $valid_language_codes)) {
-        add_settings_error('mt_options', 'mt_invalid_website_language_code', 'Invalid website language code.');
-    }
-    if (!in_array($input['translation_language_code'], $valid_language_codes)) {
-        add_settings_error('mt_options', 'mt_invalid_translation_language_code', 'Invalid translation language code.');
-    }
-
-    $api_key = $input['api_key'];
-    $url = "https://translation.googleapis.com/language/translate/v2?key=$api_key&q=hello&source=en&target=es";
-    $response = wp_remote_get($url);
-    if ( is_wp_error( $response ) ) {
-        add_settings_error('mt_options', 'mt_connection_error', 'Could not connect to the API. Please check your API key.');
-    }
-    return $input;
 }
 
 // Connect to Google Cloud Translation API and translate the posts
@@ -222,86 +297,7 @@ function translate_text($text, $target_language, $website_language, $api_key) {
     return $response_body['data']['translations'][0]['translatedText'];
 }
 
-// Use the translated posts
-function use_translated_posts() {
-    // Backup the original posts and postmeta tables
-    // Replace the original posts and postmeta tables with the translated ones
-    // Log the process
-}
-
-// Detect language code uk or ru on first 50 words of a post
-function detectLanguage($text) {
-    // Обмежуємо текст до перших 50 слів
-    $words = explode(' ', $text);
-    $text = implode(' ', array_slice($words, 0, 50));
-    // Видаляємо HTML з тексту
-    $text = wp_strip_all_tags($text);
-    // Набір унікальних слів для кожної мови
-    $ukrainianWords = array('і', 'ї', 'є', 'ґ');
-    $russianWords = array('ы', 'э', 'ё', 'й');
-    $ukCount = 0;
-    $ruCount = 0;
-
-    // Перевіряємо кількість унікальних слів для кожної мови в тексті
-    foreach ($ukrainianWords as $word) {
-        $ukCount += substr_count($text, $word);
-    }
-
-    foreach ($russianWords as $word) {
-        $ruCount += substr_count($text, $word);
-    }
-
-    // Повертаємо код мови в залежності від того, яка мова має більшу кількість унікальних слів
-    if ($ukCount > $ruCount) {
-        return 'uk';
-    } else {
-        return 'ru';
-    }
-}
-
-// Давайте проаналізуємо скільки саме у нас статей, та їх мову. Але зробимо це безпечно пачками по Х штук
-add_action('wp_ajax_analysePosts', 'analysePosts');
-function analysePosts() {
-    global $wpdb;
-    // Встановлюємо кількість статей для обробки за один раз. 
-    $batch_size = get_option('mt_batch_size', 100); // 100 - значення за замовчуванням
-    // Отримуємо кількість всіх опублікованих статей
-    $total_posts = $wpdb->get_var("SELECT COUNT(*) FROM {$wpdb->prefix}posts WHERE post_type = 'post' AND post_status = 'publish'");
-    // Розраховуємо кількість партій
-    $batches = ceil($total_posts / $batch_size);
-    $start_time = microtime(true);
-
-    for ($i = 0; $i < $batches; $i++) {
-        // Обчислюємо зсув для поточної партії
-        $offset = $i * $batch_size;
-
-        // Отримуємо статті для поточної партії
-        $posts = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}posts WHERE post_type = 'post' AND post_status = 'publish' LIMIT $offset, $batch_size");
-
-        foreach ($posts as $post) {
-            // Визначаємо мову статті
-            $language_code = detectLanguage($post->post_content);
-
-            // Додаємо дані до нової таблиці
-            $wpdb->insert(
-                "{$wpdb->prefix}sgct_analysed_posts",
-                array(
-                    'post_id' => $post->ID,
-                    'post_title' => $post->post_title,
-                    'language_code' => $language_code
-                )
-            );
-        }
-    }
-
-    $end_time = microtime(true);
-    $execution_time = $end_time - $start_time;
-
-    // Повертаємо результати через AJAX
-    echo "Аналіз завершено. Оброблено " . $total_posts . " статей. Витрачено часу " . $execution_time . " секунд.";
-    wp_die(); // це потрібно, щоб уникнути повернення 0 в кінці відповіді AJAX
-}
-
+// Налаштування кнопки Перекладу статей
 function mt_setting_translate_button() {
     // Перевіряємо, чи була натиснута кнопка
     if (isset($_POST['translate_posts'])) {
@@ -337,6 +333,13 @@ function mt_setting_translate_button() {
     });
     </script>
     ';
+}
+
+// Use the translated posts
+function use_translated_posts() {
+    // Backup the original posts and postmeta tables
+    // Replace the original posts and postmeta tables with the translated ones
+    // Log the process
 }
 
 ?>
